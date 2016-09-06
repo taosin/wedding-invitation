9753b97b0c26d548f3564c1e00198a6ec91bcfac
taoxin <taoxin167@gmail.com>
2016-09-06 19:05:22 +0800

    first commit---提交基本框架

---
+++ src/views/index.vue
@@ -0,0 +1,62 @@
+<!-- 首页 -->
+<template>
+	<div>
+    sdsdssd
+  </div>
+</template>
+<script>
+	// import toplayout from './../components/toplayout.vue';
+	export default{
+		components:{
+			// toplayout,
+		},
+		vuex: {
+			getters: {
+				// menus: state => state.leftmenus
+			},
+			actions: {
+			}
+		},
+		data(){
+			return{
+			};
+		},
+		created(){
+		},
+		ready(){
+		},
+		watch:{
+		},
+		computed:{
+		},
+		methods:{
+		}
+	};
+</script>
+<style type="text/css">
+	.topss{
+		height: 200px;
+		/*border-bottom: 1px solid #ddd;*/
+		text-align: center;
+		padding-top: 30px;
+		font-size: 24px;
+	}
+	.menu-main{
+		width: 900px;
+		margin: 0 auto;
+		text-align: right;
+		border-bottom: 1px solid #ddd;
+		height: 170px;
+	}
+	.menu-ul{
+		padding-top: 60px;
+	}
+	.menu-ul ul li{
+		float: left;
+		list-style: none;
+		margin-left: 20px;
+	}
+	.menu-logo{
+		float: left;
+	}
+</style>
